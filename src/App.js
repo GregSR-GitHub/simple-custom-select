3842237b853dedc74756cd782a629cd8eed7ea4f
Greg <greg_sr@hotmail.com>
2023-01-04 17:42:57 +0100

    functionnal, improved aria, add keyborad

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import './App.css';
 
 function App() {
   const [select, setSelect] = useState("Aucune sélection");
-  const options = ["Option 1", "Option 2", "Option 3" ];
+  const options = ["Option 1", "Option 2", {value: "Gateau", name: "Option 3"}, "Option 4" ];
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div>
-          <SimpleCustomSelect options={options}/>
-          Vous avez selectionné: {select}.
+          <SimpleCustomSelect options={options} name="test-select"/>
+          Vous avez selectionné: {JSON.stringify(select)}.
         </div>
       </header>
     </div>
--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -1,19 +1,67 @@
 import './select.css';
 import { useState } from 'react'
 
-function SimpleCustomSelect({options}) {
+function SimpleCustomSelect({name, options}) {
     const [isOpen, setIsOpen] = useState(false);
-    const [isSelected, setIsSelected] = useState(options[0]);
+    const [isSelected, setIsSelected] = useState(0);
+    let option = {};
+
+    // Selected option must be an object
+    if(typeof(options[isSelected])==="object"){
+        option = options[isSelected]; 
+    }else{
+        option = {value: options[isSelected], name: options[isSelected]};
+    }
+
+    // If no name use this default one
+    if(!name){
+        name = "custom-select";
+    }
+
+    // Enter Keyboard can select an option
+    function handleEnter(index,event){
+        if(event.key==="Enter") {
+            event.preventDefault();
+            setIsSelected(index);
+            setIsOpen(false);
+        }
+    };
+
     return (
-        <div className="select-wrapper">
-            <button className="select-button"  onClick={(e) => setIsOpen(!isOpen)}>
-                {isSelected}
-            <span  className="select-arrow"></span>
+        <div id={name + "-wrapper"} className="custom-select-wrapper">
+            <button 
+            id={name + "-button"} 
+            className="custom-select-button" 
+            value={option.value} 
+            aria-haspopup="listbox" 
+            aria-expanded={isOpen}  
+            aria-labelledby={name + "-option" + isSelected}
+            onClick={(e) => setIsOpen(!isOpen)}
+            >
+                {option.name}
+                <span  className="custom-select-arrow"></span>
             </button>
             {isOpen&&
-            <ul className="select-list">
-                { options.map((element, index) => (<li className="select-option" key={`${element}-${index}`}  onClick={(e) => {setIsOpen(false); setIsSelected(element)}}>{element}</li>))}
-            </ul>}
+            <ul  id={name + "-list"} role="listbox" className="custom-select-list" aria-activedescendant={name + "-option" + isSelected} tabIndex={-1}>
+                { options.map((element, index) => (
+                <li 
+                    id={name + "-option" + index}
+                    className="custom-select-option" 
+                    key={`${name}-${index}`} 
+                    role="option" 
+                    aria-selected={isSelected === index}  
+                    tabIndex={0} 
+                    onKeyDown={(e) => {handleEnter(index,e)}}
+                    onClick={(e) => {
+                        setIsSelected(index);
+                        setIsOpen(false);}
+                    }
+                >
+                    {element.name?element.name:element}
+                </li>
+                ))}
+            </ul>
+            }
         </div>
     );
  }
